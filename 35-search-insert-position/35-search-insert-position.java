db518e06604f39aade4664862832c856c459adea
Anish Saraogi <asaraogi98@gmail.com>
2022-09-18 21:33:22 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (27.03%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int mid = n/2;
+        int ll = 0;
+        int ul = n-1;
+        while(ll<=ul){
+            if(nums[mid]>target){
+                ul = mid-1;
+            } else if(nums[mid]<target){
+                ll = mid+1;
+            } else if(nums[mid]==target){
+                return mid;
+            }
+            mid = (ul+ll)/2;
+        }
+        if(target<nums[mid])
+            return mid;
+        else
+            return mid+1;
+    }
+}
